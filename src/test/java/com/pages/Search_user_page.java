a7309acd56afc43825894dacf524f86c66f75241
Mylambigai <mylambigai16@gmail.com>
2026-05-18 21:06:15 +0530

    Search_user_page

--- src/test/java/com/pages/Search_user_page.java
+++ src/test/java/com/pages/Search_user_page.java
@@ -1,32 +1,44 @@
 package com.pages;
 
+import java.util.List;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class Search_user_page extends BasePage {
 
-	
-	@FindBy(xpath="//a[@class='oxd-main-menu-item active']")
-	public WebElement Admin_page;
-	
-	@FindBy(xpath="//div[@class=\"oxd-input-group__label-wrapper\"]/following-sibling::div/input")
-	public WebElement username_field;
-	
-	@FindBy(xpath="//div[@class='oxd-select-text oxd-select-text--focus']//div[@class='oxd-select-text-input'][normalize-space()='-- Select --']")
-	public WebElement userRole;
-	
-	@FindBy(xpath="//input[@placeholder='Type for hints...']")
-	public WebElement employeeName;
-	
-	@FindBy(css="button[type='submit']")
-	public WebElement save_btn;
-	
-	@FindBy(xpath="//div[@role='rowgroup']//div[1]//div[1]//div[2]//div[1]")
-	public WebElement username_record;
-	
-	@FindBy(xpath="//div[@role='rowgroup']//div[1]//div[1]//div[3]//div[1]")
-	public WebElement userrole_record;
-	
-	@FindBy(xpath="//div[@role='rowgroup']//div[1]//div[1]//div[4]//div[1]")
-	public WebElement employeeName_record;
+    @FindBy(xpath = "//a[@class='oxd-main-menu-item active']")
+    public WebElement Admin_page;
+
+    @FindBy(xpath = "//div[@class=\"oxd-input-group__label-wrapper\"]/following-sibling::div/input")
+    public WebElement username_field;
+
+    @FindBy(xpath="//h5[@class='oxd-text oxd-text--h5 oxd-table-filter-title']")
+    public WebElement system_user;
+    
+    @FindBy(xpath = "//label[normalize-space()='User Role']/../..//div[@class='oxd-select-text-input']")
+    public WebElement userRole;
+
+    @FindBy(xpath = "//div[@role='listbox']//div[@role='option']")
+    public List<WebElement> userRoleOptions;
+
+    @FindBy(xpath = "//input[@placeholder='Type for hints...']")
+    public WebElement employeeName;
+
+    @FindBy(css = "button[type='submit']")
+    public WebElement search_btn;
+
+    @FindBy(xpath = "//div[@role='rowgroup']//div[1]//div[1]//div[2]//div[1]")
+    public WebElement username_record;
+
+    @FindBy(xpath = "//div[@role='rowgroup']//div[1]//div[1]//div[3]//div[1]")
+    public WebElement userrole_record;
+
+    @FindBy(xpath = "//div[@role='rowgroup']//div[1]//div[1]//div[4]//div[1]")
+    public WebElement employeeName_record;
+
+    @FindBy(xpath = "//div[@role='listbox']")
+    public WebElement listbox;
+
+    @FindBy(xpath = "//div[@role='listbox']//div[@role='option']")
+    public List<WebElement> employeeSuggestions;
 }
