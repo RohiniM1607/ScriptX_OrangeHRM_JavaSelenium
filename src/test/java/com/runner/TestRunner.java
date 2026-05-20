b055721ef2b661f4eea02c419b4eed27c1bf336f
RESHMA1104 <163125056+RESHMA1104@users.noreply.github.com>
2026-05-20 19:37:19 +0530

    Update test tags in TestRunner class

--- src/test/java/com/runner/TestRunner.java
+++ src/test/java/com/runner/TestRunner.java
@@ -20,7 +20,7 @@ import io.cucumber.testng.CucumberOptions;
         },
         monochrome = true,
         publish = true
-        //tags = "@EmployeeEntitlements"
+        tags = "@Reshmavalid"
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
 
