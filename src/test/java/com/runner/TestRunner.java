d0e9dc6f7c41533c9d15e8d99d650f5ec09cfebc
RohiniM1607 <mrohini7558@gmail.com>
2026-05-29 06:55:20 +0530

    Test Data Changed

--- src/test/java/com/runner/TestRunner.java
+++ src/test/java/com/runner/TestRunner.java
@@ -6,9 +6,9 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-		features = "src/test/resources/features/CreateUserCredentials.feature", 
-
+		features = "src/test/resources/features/", 
         glue = {"com.stepdefinitions", "com.hooks" }, 
+        tags = "@Rohini",
         plugin = { "pretty", "html:target/CucumberReports/Cucumber.html",
 				        "json:target/CucumberReports/Cucumber.json",
 				        "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
