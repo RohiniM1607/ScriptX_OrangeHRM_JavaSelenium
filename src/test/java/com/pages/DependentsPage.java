ac5e3d35429db796d915526297b49042e29ae1c3
RESHMMA1104 <resh1104mcdc@gmail.com>
2026-06-08 19:32:30 +0530

    Feature added

--- src/test/java/com/pages/DependentsPage.java
+++ src/test/java/com/pages/DependentsPage.java
@@ -29,4 +29,25 @@ public class DependentsPage extends BasePage {
 
     @FindBy(xpath = "//div[@role='listbox']//span")
     public List<WebElement> dropdownOptions;
+    
+    @FindBy(xpath = "//div[h6[text()='Attachments']]//button[normalize-space()='Add']")
+    public WebElement attachmentAddButton;
+
+    @FindBy(xpath = "//input[@type='file']")
+    public WebElement fileInput;
+
+    @FindBy(xpath = "//label[text()='Comment']/following::textarea[1]")
+    public WebElement commentInput;
+
+    @FindBy(xpath = "//button[@type='submit']")
+    public WebElement saveButton1;
+
+    @FindBy(xpath = "//button[normalize-space()='Cancel']")
+    public WebElement cancelButton;
+
+    @FindBy(xpath = "//div[contains(@class,'oxd-toast ')]//p[contains(@class,'oxd-text')]")
+    public WebElement successMessage1;
+
+    @FindBy(xpath = "//span[contains(@class,'oxd-input-field-error-message') or contains(text(),'Attachment Size Exceeded')]")
+    public WebElement fileSizeErrorMessage;
 }
