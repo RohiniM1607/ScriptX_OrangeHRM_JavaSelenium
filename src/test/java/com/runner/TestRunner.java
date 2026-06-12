088bbcf61442529c72b879813109091e0c08f3aa
Subhashree <subhashreerajait@gmail.com>
2026-06-12 10:19:31 +0530

    "updated"

--- src/test/java/com/runner/TestRunner.java
+++ src/test/java/com/runner/TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-		features = "src/test/resources/features/", 
+		features = "src/test/resources/features/ApplyLeave_Employee.feature", 
         glue = {"com.stepdefinitions", "com.hooks" }, 
         plugin = { "pretty", "html:target/CucumberReports/Cucumber.html",
 				        "json:target/CucumberReports/Cucumber.json",
