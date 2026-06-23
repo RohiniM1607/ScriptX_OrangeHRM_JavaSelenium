14289b156b06eabee710b21efc50fb7cceba6d0b
Jagadeep2004 <jagadeep2004@gmail.com>
2026-06-23 11:23:53 +0530

    Update feature file path in TestRunner

--- src/test/java/com/runner/TestRunner.java
+++ src/test/java/com/runner/TestRunner.java
@@ -7,7 +7,7 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
 
-        features = "src/test/resources/features/LeaveList_Employee.feature",
+        features = "src/test/resources/features/",
         glue = {"com.stepdefinitions", "com.hooks"},
         plugin = {
                 "pretty",
