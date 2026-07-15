65b7f2a69200705eed699ba3eba0bd03871217cd
ROHINI M <mrohini7558@gmail.com>
2026-07-16 01:27:45 +0530

    Update TestRunner.java

--- src/test/java/com/runner/TestRunner.java
+++ src/test/java/com/runner/TestRunner.java
@@ -5,23 +5,21 @@ import org.testng.annotations.DataProvider;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions( 
+@CucumberOptions(
+		features = "src/test/resources/features/", 
         glue = {"com.stepdefinitions", "com.hooks" }, 
+        tags = "@Rohini",
         plugin = { "pretty", "html:target/CucumberReports/Cucumber.html",
 				        "json:target/CucumberReports/Cucumber.json",
 				        "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-				        "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm" },
-        features = "src/test/resources/features/",
-        monochrome = true,
-        tags = "@LeaveList"
-        )
-
+				        "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm" }, 
+        monochrome = true, 
+        publish = true)
 public class TestRunner extends AbstractTestNGCucumberTests {
 
-    @Override
-    @DataProvider(parallel = true)
-    public Object[][] scenarios() {
-        return super.scenarios();
-    }
-
+	@Override
+	@DataProvider(parallel = false)
+	public Object[][] scenarios() {
+		return super.scenarios();
+	}
 }
